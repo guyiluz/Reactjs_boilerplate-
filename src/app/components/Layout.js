ba2b7c02cd9fa9a520bbeef5f861edcd6a46ba65
Guy home <guyi@hereo.co.uk>
2017-09-29 12:55:18 +0300

    First Commit

---
+++ src/app/components/Layout.js
@@ -0,0 +1,34 @@
+// lib
+import React from 'react'
+// components
+
+ 
+// other
+import {apiCall}  from '../js/api'
+
+class Layout extends React.Component {
+  
+
+
+
+
+
+
+
+
+
+
+    
+    render() {
+
+        return (
+            <div className="layout">
+      <h1>This a boilerpate</h1>
+          </div>  
+          
+        )
+    }
+}
+
+export default Layout;
+
